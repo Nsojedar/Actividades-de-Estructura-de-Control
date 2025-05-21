ffab0470ed1a2d164f50f79aa848afd0afcf4fc4
Nsojedar <nsojeda@espe.edu.ec>
2025-05-21 13:54:41 -0500

    Create tablaMultiplicar.js

---
+++ Actividad/tablaMultiplicar.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Actividad 2 - Ingresa un número para la tabla de multiplicar:"));
+console.log("Actividad 2: Tabla de Multiplicar");
+
+for (let i = 1; i <= 12; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
